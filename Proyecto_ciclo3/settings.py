da66187927d09f36187dae54dab9cb9072f01290
Solrac2112 <carlos.bermudez19960221@gmail.com>
2022-09-03 20:16:07 -0500

    Proyecto_V1.5

--- Proyecto_ciclo3/settings.py
+++ Proyecto_ciclo3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Supermercado'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Proyecto_ciclo3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'PASSWORD':'Solrac_1234567890AB',
+        'HOST':'db.jvhtualsjdpaywuhtboo.supabase.co',
+        'PORT':'5432'
     }
 }
 
