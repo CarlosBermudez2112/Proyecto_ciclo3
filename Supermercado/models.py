907c7c491c12d4f7909a1bd90b946521c6b7c2fd
CarlosMoreno1708 <ing.karlos.17@gmail.com>
2022-09-04 22:00:57 -0500

    Proyecto_V1.6

--- Supermercado/models.py
+++ Supermercado/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
@@ -22,5 +23,25 @@ class EmployeePayroll(models.Model):
     PAY_StartDate=models.DateField()
     PAY_FinalDate=models.DateField()
     PAY_TotalSalary=models.IntegerField(null=True)
+
+class CUSTOMERS(models.Model):
+    CLI_User = models.TextField(max_length=25,null=False,unique=True)
+    CLI_Password=models.IntegerField(null=False)
+    CLI_Email = models.EmailField(unique=True)
+    CLI_Names = models.TextField(max_length=50,null=False)
+    CLI_LastNames= models.TextField(max_length=50,null=False)
+    CLI_CellPhone= models.TextField(max_length=50,unique=True)
+    CLI_AD_User= models.ForeignKey(ADMINISTRATOR, on_delete=models.CASCADE)
+
+class EXPENSES(models.Model):
+    EGR_Code = models.CharField(10,null=False,unique=True)
+    EGR_EM_NIT = models.ForeignKey(BUSINESS, on_delete=models.CASCADE)
+    EGR_TEGR_Code = models.ForeignKey(TypeExpenses, on_delete=models.CASCADE)
+    EGR_Fecha = models.DateField(auto_now=True)
+    EGR_Total = models.IntegerField(max_length=50,null=False)
+
+class TypeExpenses(models.Model):
+    TEGR_Code = models.CharField(max_length=10,null=False,unique=True)
+    TEGR_NameExpenses = models.IntegerField(max_length=10,null=False,unique=True)
     
     
