a59f0fb335e3782b68649a407a3d6130c9cebeb8
Solrac2112 <carlos.bermudez19960221@gmail.com>
2022-09-05 21:12:48 -0500

    Proyecto_V1.9

--- Supermercado/models.py
+++ Supermercado/models.py
@@ -7,7 +7,7 @@ from django.db import models
 ## Creación de la tabla administradores junto con sus campos
 
 class ADMINISTRATOR(models.Model):
-    AD_USER=models.TextField(max_length=50,null=False,unique=True)
+    AD_USER=models.TextField(primary_key=True, max_length=50,null=False,unique=True)
     AD_PASSWORD=models.IntegerField(max_length=10)
     AD_EMAIL=models.TimeField(max_length=50,null=False)
     AD_NAMES=models.TextField(max_length=50,null=False)
@@ -20,7 +20,7 @@ class ADMINISTRATOR(models.Model):
 class BUSINESS(models.model):
     EM_ID=models.BigIntegerField(max_length=50,null=False)
     EM_IDName=models.TextField(max_length=50,null=False)
-    EM_NIT=models.IntegerField(max_length=10,unique=True)
+    EM_NIT=models.IntegerField(primary_key=True,max_length=10,unique=True)
     EM_CITY=models.TextField(max_length=50)
     EM_ADDRESS=models.TextField(max_length=50)
     EM_CELLPHONE=models.TextField(max_length=10)
@@ -31,7 +31,7 @@ class BUSINESS(models.model):
 ## Creación de la tabla empleados junto con sus campos
 
 class EMPLOYEES(models.model):
-    EMP_USER=models.TextField(max_length=50,null=False,unique=True)
+    EMP_USER=models.TextField(primary_key=True,max_length=50,null=False,unique=True)
     EMP_PASSWORD=models.IntegerField(max_length=10)
     EMP_EMAIL=models.TimeField(max_length=50,null=False)
     EMP_NAMES=models.TextField(max_length=50,null=False)
@@ -43,14 +43,14 @@ class EMPLOYEES(models.model):
 
 
 class WORKINGHOURS(models.Model):
-    WORH_Code=models.TextField(max_length=50,null=True,unique=True)
+    WORH_Code=models.TextField(primary_key=True, max_length=50,null=True,unique=True)
     WORH_TipeHours=models.TextField(max_length=50,null=True,unique=True)
     WORH_Costs=models.IntegerField(null=True)
     
     
 class EmployeePayroll(models.Model):
-    PAY_Id=models.IntegerField(auto_created=True, Unique=True)
-    PAY_EM_User=models.ForeignKey(Employees, on_delete=models.CASCADE)
+    PAY_Id=models.IntegerField(primary_key=True, auto_created=True, Unique=True)
+    PAY_EM_User=models.ForeignKey(EMPLOYEES, on_delete=models.CASCADE)
     PAY_NIT=models.ForeignKey(BUSINESS, on_delete=models.CASCADE)
     PAY_Hours=models.IntegerField(null=True)
     PAY_ExtraHours=models.IntegerField(null=True)
@@ -61,7 +61,7 @@ class EmployeePayroll(models.Model):
     PAY_TotalSalary=models.IntegerField(null=True)
 
 class CUSTOMERS(models.Model):
-    CLI_User = models.TextField(max_length=25,null=False,unique=True)
+    CLI_User = models.TextField(primary_key=True, max_length=25,null=False,unique=True)
     CLI_Password=models.IntegerField(null=False)
     CLI_Email = models.EmailField(unique=True)
     CLI_Names = models.TextField(max_length=50,null=False)
@@ -72,11 +72,11 @@ class CUSTOMERS(models.Model):
 
 
 class TypeExpenses(models.Model):
-    TEGR_Code = models.CharField(max_length=10,null=False,unique=True)
+    TEGR_Code = models.CharField(primary_key=True, max_length=10,null=False,unique=True)
     TEGR_NameExpenses = models.IntegerField(max_length=10,null=False,unique=True)
     
 class EXPENSES(models.Model):
-    EGR_Code = models.CharField(10,null=False,unique=True)
+    EGR_Code = models.CharField(primary_key=True, max_length=10,null=False,unique=True)
     EGR_EM_NIT = models.ForeignKey(BUSINESS, on_delete=models.CASCADE)
     EGR_TEGR_Code = models.ForeignKey(TypeExpenses, on_delete=models.CASCADE)
     EGR_Fecha = models.DateField(auto_now=True)
