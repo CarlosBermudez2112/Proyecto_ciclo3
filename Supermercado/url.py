52537c36b0d92555188abc27792bda968248ecd0
Solrac2112 <carlos.bermudez19960221@gmail.com>
2022-09-13 20:31:33 -0500

    proyecto_V2.19

--- Supermercado/url.py
+++ Supermercado/url.py
@@ -2,18 +2,18 @@ from django.urls import path
 from Supermercado.views import *
 urlpatterns = [
     path('ADMINISTRATOR/',ADMINISTRATORView.as_view(),name='ListarAdministrador'),
-    path('ADMINISTRATOR/<str:dato>',ADMINISTRATORView.as_view(),name='ListarAdministradorPorUsuario'),
+    path('ADMINISTRATOR/<str:AD_USER>',ADMINISTRATORView.as_view(),name='ListarAdministradorPorUsuario'),
     path('BUSINESS/',BUSINESSView.as_view(),name='ListarBusiness'),
-    path('BUSINESS/<str:dato>',BUSINESSView.as_view(),name='ListarBusiness'),
+    path('BUSINESS/<int:EM_nit>',BUSINESSView.as_view(),name='ListarBusiness'),
     path('Employeepayroll/',EMPLOYEEPAYROLLView.as_view(),name='Listarnomina'),
-    path('Employeepayroll/<str:dato>',EMPLOYEEPAYROLLView.as_view(),name='ListarID'),
+    path('Employeepayroll/<int:PAY_id>',EMPLOYEEPAYROLLView.as_view(),name='ListarID'),
     path('WORKINGHOURS/',EMPLOYEEPAYROLLView.as_view(),name='Listarhours'),
-    path('WORKINGHOURS/<str:dato>',EMPLOYEEPAYROLLView.as_view(),name='ListarHo'),
+    path('WORKINGHOURS/<str:WORH_code>',EMPLOYEEPAYROLLView.as_view(),name='ListarHo'),
     path('CUSTOMERS/',CUSTOMERSView.as_view(),name='ListarCliente'),
-    path('CUSTOMERS/<str:dato>',CUSTOMERSView.as_view(),name='ListarClientePorUsuario'),
+    path('CUSTOMERS/<str:user>',CUSTOMERSView.as_view(),name='ListarClientePorUsuario'),
     path('LISTBUY/',LISTBUYView.as_view(),name='ListarListCompra'),
-    path('LISTBUY/<str:dato>',LISTBUYView.as_view(),name='ListarPorCodigo'),
+    path('LISTBUY/<int:LBUY_code>',LISTBUYView.as_view(),name='ListarPorCodigo'),
     path('TYPEEXPENSES/',TYPEEXPENSESView.as_view(),name='ListarTiposCobros'),
-    path('TYPEEXPENSES/<str:dato>',TYPEEXPENSESView.as_view(),name='ListarTipoCobros'),    
+    path('TYPEEXPENSES/<str:code>',TYPEEXPENSESView.as_view(),name='ListarTipoCobros'),    
 
 ]
